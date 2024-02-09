e35d848dd48a39b42c8588340871b9b029287e65
Thuraitun <thuraitun64@gmail.com>
2024-02-09 21:44:41 +0630

    added error to fetch api

--- app/products/api/route.ts
+++ app/products/api/route.ts
@@ -2,7 +2,7 @@ import { query } from "@/app/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: any) {
-    const sql = "SELECT * FROM products ORDER BY Id DESC";
+    const sql = "SELECT * FROM products ORDER BY id DESC";
     const result = await query(sql, "")
 
     try {
